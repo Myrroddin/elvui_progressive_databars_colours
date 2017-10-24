79dfac08304c90946ba3ad55286ccd9ffad83bf4
myrroddin <myrroddin@fa23aac1-e5bc-4d57-b0ea-6242d1ac6797>
2017-10-24 10:09:28 +0000

    - Apparently some optimizations are too optimal and they prevent colorization from working. Reverted a few of them.

--- trunk/Honor.lua
+++ trunk/Honor.lua
@@ -1,9 +1,9 @@
 local E, L, V, P, G = unpack(ElvUI) -- import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
 local EDB = E:GetModule("DataBars") -- ElvUI's DataBars
 local PCB = E:GetModule("PCB") -- this AddOn
-local bar = EDB.honorBar -- less typing
 
-local function UpdateHonor()
+local function UpdateHonor(self)
+    local bar = self.honorBar
     local color = E.db.PCB.honorBar.color
     bar.statusBar:SetStatusBarColor(color.r, color.g, color.b)
 
@@ -17,7 +17,7 @@ local function UpdateHonor()
 end
 
 function PCB:HookHonorBar()
-    if E.db.PCB.enabled and bar then
+    if E.db.PCB.enabled and EDB.honorBar then
         if not PCB:IsHooked(EDB, "UpdateHonor") then
             PCB:SecureHook(EDB, "UpdateHonor", UpdateHonor)
         end
@@ -31,6 +31,7 @@ function PCB:HookHonorBar()
 end
 
 function PCB:RestoreHonorBar()
+    local bar = EDB.honorBar
     if bar then
         bar.statusBar:SetStatusBarColor(240/255, 114/255, 65/255)
         bar.statusBar:SetAlpha(1)
