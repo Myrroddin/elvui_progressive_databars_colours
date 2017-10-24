34bc311c076d64e9cf3c37059303c9b6fb1582cc
myrroddin <myrroddin@fa23aac1-e5bc-4d57-b0ea-6242d1ac6797>
2017-10-24 12:04:08 +0000

    - Fix a couple of typos - Use a better method of check if an artifact weapon or fishing pole is equipped - Process the blend math only if the toggle is enabled

--- trunk/Artifact.lua
+++ trunk/Artifact.lua
@@ -3,24 +3,24 @@ local EDB = E:GetModule("DataBars") -- ElvUI's DataBars
 local PCB = E:GetModule("PCB") -- this AddOn
 
 local function UpdateArtifact(self)
+    if not HasArtifactEquipped() then
+        -- no artifact weapon or fishing pole equipped, exit out
+        return
+    end
+
     local bar = self.artifactBar
     local aColor = E.db.PCB.artifactBar.artColor
-
-    local current = select(5, C_ArtifactUI.GetEquippedArtifactInfo())
-
-    -- exit out for characters with no artififact weapon equipped
-    if not current then return end
-
-    local numTraits = select(6, C_ArtifactUI.GetEquippedArtifactInfo())
-    local tier = select(13, C_ArtifactUI.GetEquippedArtifactInfo())
-    local nextCost = C_ArtifactUI.GetCostForPointAtRank(numTraits, tier)
-
-    local avg = current / nextCost
-    avg = PCB:Round(avg, 2)
 
     bar.statusBar:SetStatusBarColor(aColor.r, aColor.g, aColor.b)
 
     if E.db.PCB.artifactBar.progress then
+        local current = select(5, C_ArtifactUI.GetEquippedArtifactInfo())
+        local numTraits = select(6, C_ArtifactUI.GetEquippedArtifactInfo())
+        local tier = select(13, C_ArtifactUI.GetEquippedArtifactInfo())
+        local nextCost = C_ArtifactUI.GetCostForPointAtRank(numTraits, tier)
+
+        local avg = current / nextCost
+        avg = PCB:Round(avg, 2)
         bar.statusBar:SetAlpha(avg)
     else
         bar.statusBar:SetAlpha(1)
@@ -34,7 +34,7 @@ function PCB:HookArtifactBar()
         end
     elseif not E.db.PCB.enabled or not EDB.artifactBar then
         if PCB:IsHooked(EDB, "UpdateArtifact") then
-            PCB:UnHook(EDB, "UpdateArtifact")
+            PCB:Unhook(EDB, "UpdateArtifact")
         end
         PCB:RestoreArtifactBar()
     end
@@ -44,7 +44,7 @@ end
 function PCB:RestoreArtifactBar()
     local bar = EDB.artifactBar
     if bar then
-        bar:SetStatusBarColor(.901, .8, .601)
-        bar:SetAlpha(1)
+        bar.statusBar:SetStatusBarColor(.901, .8, .601)
+        bar.statusBar:SetAlpha(1)
     end
 end
